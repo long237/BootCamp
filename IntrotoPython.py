63ee000bbd39d7fa7e1610f13838f296cd0cf091
Lunastra <ln24630@gmail.com>
2021-06-05 23:31:43 -0700

    Initialize introduction file

--- IntrotoPython.py
+++ IntrotoPython.py
@@ -4,5 +4,39 @@
 #Any line with the symbol "#", at the begining will be ignored by Python
 #Such as this one
 
-#Print Statement
+#Print Statements
+print("Hello World")
+print('Hello World')
+
+#Numbers and variables
+#Just like in math, you can save values such as number into variables
+
+x = 5       #integer
+y = 2.64    #float
+z = 3.14
+
+#Printing numbers to the console
+
+print("Value of x:", x)
+
+#Saving Strings to a variable
+
+a = "Hello"
+b = "Long"
+
+#Combingning and printing Strings (String Concatenation)
+#What would be the value of c ?
+
+c = a + b
+print(c)
+print("Hello" + b)
+print("hello" + "Long")
+
+
+
+
+
+
+
+
 
