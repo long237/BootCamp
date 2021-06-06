7b4ac976bff53befc47189df890444102477d4f2
Lunastra <ln24630@gmail.com>
2021-06-05 23:09:58 -0700

    Initial commit

---
+++ IntrotoPython.py
@@ -0,0 +1,8 @@
+#Introduction to Python
+
+#Comments
+#Any line with the symbol "#", at the begining will be ignored by Python
+#Such as this one
+
+#Print Statement
+
---
+++ testFile.py
@@ -0,0 +1,21 @@
+ar = [1, 5, 6, 7]
+ar2 = []
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+print(ar)
+print("An empty array ", ar2)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+print(thisdict["model"])
+#print(thisdict.get("model")
+
+print(thisdict.keys())
+
+y = 3 / 2
+print ("This is value: ", y)
