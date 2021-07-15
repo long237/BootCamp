e997f2d68c2aff534dab8fefc74b77dc87bfc97a
Lunastra <ln24630@gmail.com>
2021-07-15 15:22:47 -0700

    Initialized Precedence file

---
+++ Precedence.py
@@ -0,0 +1,57 @@
+#Order of Precedence in Python
+
+#Mathematical operations follow the same order as when you do math.
+
+#Also known as PEMDAS:
+
+#1) Parenthesization
+
+#2) Factorial
+
+#3) Exponential
+
+#4) Multiplication and division
+
+#5) Addition and substraction
+
+#Note: "**" operator is exponential in Python
+
+#Ex1: What would be the value of a?
+a = 3 - 2 * 5
+print("Value of a:", a)
+
+#Ex2:
+b = 4 ** 2 / 2
+print("Value of b:", b)
+
+#Ex3:
+c = 4 * ( 2 + 3 ) ** 2
+print("Value of c:", c)
+
+#Ex4:
+d = (3 - 1) ** 2 + 4 ** 2 * 2
+print("Value of d:", d)
+
+#Ex5:
+e = (5 - 1) * 3 + 15 / 3
+print("Value of e:", e)
+
+#Ex6:
+f = (5 - 3) ** (2 * (5 - 3))
+print("Value of f:", f)
+
+#Ex7:
+g = (8 - 5) - 8 ** (1 / 3)
+print("Value of g:", g)
+
+#Ex8:
+h = 2 * 2 ** 5 / 8 
+print("Value of h:", h)
+
+#Ex9:
+i = (4 + 12) / 2 ** 2
+print("Value of i:", i)
+
+#Ex10:
+j = 2 + 3 ** 2 + (2 + 4) ** 2 / 2
+print("Value of j:", j)
