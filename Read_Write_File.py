0b8e081a0e0c527d2ee6634d148124ab4ee8c8b4
Lunastra <ln24630@gmail.com>
2021-07-19 16:49:18 -0700

    Add writing to a file code

--- Read_Write_File.py
+++ Read_Write_File.py
@@ -21,12 +21,44 @@
 # 4)Write and read ('w+')
 # 5)Append only ('a')
 # 6)Append and read ('a+')
+
+
 myfile = open("randomNames.txt", "r")
 
-#print(myfile.readline())
+#Using readline() to read one line from the file:
+print(myfile.readline())
 
+#Reading the entire file line by line:
 for name in myfile:
     print(name)
+
+#Reading from a file just give you a string so you can do anything with it
+#print("Hello", myfile.readline())
+
+#Closing the file:
+#It is a good practice to close the file to prevent unexpected things to happen when you code. 
+myfile.close()
+
+
+
+
+# oOo 2)Writing information to a file: oOo:
+
+#When writing to the file using the open function, you should becareful when determining if you should use append or write
+#   1)Append: add information to the end of the file
+#   2)Write: Overwrite any information that already exist in the file with the new information
+
+file = open("userFile.txt", "a")
+
+#Writing to a file:
+file.write("Go Beach \n")
+
+#Adding more lines to the file:
+file.write("Shark is CSULB brand new mascot \n")
+
+#Closing the file after modifying the file
+file.close()
+
 
 
 
