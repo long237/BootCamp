933e37f49bae266f68a8b7fc82465e54abec26fe
Lunastra <ln24630@gmail.com>
2021-07-28 21:17:05 -0700

    Comment out print statements

--- IntrotoPython.py
+++ IntrotoPython.py
@@ -4,7 +4,7 @@
 #Any line with the symbol "#", at the begining will be ignored by Python
 #Such as this one
 
-#Print Statements
+# 1)Print Statements
 print("Hello World")
 print('Hello World')
 
@@ -29,16 +29,16 @@ s = "3.14"  #String
 
 #Printing numbers to the console
 
-print("Value of x:", x)
+##print("Value of x:", x)
 
 #What would be printed if we try to print "u" and "v"
 
-print("Value of u: ", u)
-print("Value of v: ", v)
+##print("Value of u: ", u)
+##print("Value of v: ", v)
 
 #Reassigning variables and how it works.
 x = 99
-print("Value of x:", x)
+##print("Value of x:", x)
 
 #Saving Strings to a variable
 
@@ -49,11 +49,11 @@ b = "Long"
 #What would be the value of c ?
 
 c = a + b
-print(c)
-print("Hello" + b)
-print("hello" + "Long")
+##print(c)
+##print("Hello" + b)
+##print("hello" + "Long")
 
-#Python Operators
+# 2)Python Operators
 #Python contains your normal math operators which are straight forward
 #and does what you expect them too
 
@@ -66,45 +66,46 @@ s = "2"
 #Addition
 
 p = m + n
-print("Value of p:", p)
+##print("Value of p:", p)
 
 #What happened when we add 2 strings?
 p = r + s
-print("Value of p:", p)
+##print("Value of p:", p)
 
 #How would we solve this problem??
 #You can change the data type from one to another using "Casting"
 
 #Subtraction
 p = m - n
-print("Value of p:", p)
+##print("Value of p:", p)
 
 #Multiplication
 p = m * n
-print("Value of p:", p)
+##print("Value of p:", p)
 
 #Division
 p = m / n
-print("Value of p:", p)
+##print("Value of p:", p)
 
 #Exponential
 p = m ** n
-print("Value of p:", p)
+##print("Value of p:", p)
 
 #Modifying a variable then assign it to itself
 z = 1
-print("Value of z: ", z)
+##print("Value of z: ", z)
 
 
 z = z + 1
 #What would be the value of Z after this operation?
-print(z)
+
+##print(z)
 
 #Although it looks wrong mathemtically since "z" cannot be equal to "z + 1"
 #This statement is saying that take the "z" value previously, add one to it then save it back to "z"
 
 
-#Comparison operators
+# 3) Comparison operators
 #Aside from operators that are used for math and string concatenations,
 #there are also operators for comparing value between variables
 
@@ -116,10 +117,10 @@ k = 8
 
 #Equal comparisons and not equal
 L = (j == k)
-print("Value of L:", L)
+##print("Value of L:", L)
 
 L = (j != k)
-print("Value of L:", L)
+##print("Value of L:", L)
 
 
 #Since the operator "=" or equal sign is used to assign a value to a variable
@@ -135,28 +136,28 @@ L = (j <= k)
 L = (j > k)
 L = (j >= k)
 
-#Logial operators such as And, Or, Not
+# 4)Logial operators such as And, Or, Not
 #There are words in Python that are reserved for functions and operators such as these
 #What if you want to do more than one comparison on one single line?
 
 x = 4
 
 shark = x < 10 and x > 2
-print("Value of shark: ", shark)
+##print("Value of shark: ", shark)
 
 shark = x < 10 or x > 2
 
 shark = not(x < 10)
 
 
-#Python If...else statements (also know as Conditional statement)
+# 5)Python If...else statements (also know as Conditional statement)
 #What if you want a certain things to happen when a condition is met??
 
 #Print "Go Beach" if i > 5
 
-i = 100
-if i > 5:
-    print("Go Beach")
+##i = 100
+##if i > 5:
+##    print("Go Beach")
 
 #You can also do more in the "if block" such as adding another print line 
 
@@ -170,35 +171,38 @@ if i > 5:
 #Indentation in Python is very important since it signified what statement will be executed
 #depends on which condition is met. You will get an error
     
-i = 99
-if i > 200:
-    print ("Hello World")
-elif i > 10:
-    print ("Go Beach")
-else:
-    print ("GME to the moon")
+##i = 99
+##if i > 200:
+##    print ("Hello World")
+##elif i > 10:
+##    print ("Go Beach")
+##else:
+##    print ("GME to the moon")
 
+
+#6)Python loops:
+    
 #For loops.
 #Let say you want to print "Hello World" 5 times, how would you do it without writing 5 "print" statement?
 #Also becareful with the value of "i" when you work with with loops and the range() function
 
-for i in range(5):
-        #print("Value of i:", i)
-        print("Hello World")
+##for i in range(5):
+##        #print("Value of i:", i)
+##        print("Hello World")
 
 #The same can be achieve using a while loop:
 
-i = 0
-while i < 5:
-    print("Go Beach")
-    i = i + 1 
+##i = 0
+##while i < 5:
+##    print("Go Beach")
+##    i = i + 1 
 
 #Functions for user input:
 #The function will return a String when the user enter a value which means if you are asking for a number
 #and you want to do math with it, you will have to cast it to an interger or a float
 
-name = input("Enter your name: ")
-print ("Hello, " + name)
+##name = input("Enter your name: ")
+##print ("Hello, " + name)
     
 
 
